77a25e329ae6fa982e73222240752855fad598f6
OlgaBorovik <jkz8686@gmail.com>
2022-11-04 14:15:45 +0200

    Update ContactsForm.jsx

--- src/components/ContactsForm/ContactsForm.jsx
+++ src/components/ContactsForm/ContactsForm.jsx
@@ -1,9 +1,84 @@
 import { nanoid } from "nanoid";
 import PropTypes from "prop-types"
 import React from "react";
+import { useState } from "react";
 import {Form, Label, Input, Button} from "./ContactForm.styled"
 
-class ContactsForm extends React.Component {
+const ContactsForm = ({ onSubmitProp }) => {
+    const [name, setName] = useState('')
+    const [id, setId] = useState('')
+    const[number, setNumber] = useState('')
+
+    
+    const handleInputChange = e => {
+        const { name, value } = e.currentTarget
+        switch (name) {
+            case 'name':
+                setName(value)
+                break;
+            case 'number':
+                setNumber(value)
+                break;
+            default:
+                return
+        }
+        setId(nanoid(5))
+}
+
+
+    const reset = () => {
+        setName('')
+        setNumber('')
+    
+  }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        onSubmitProp({name, number, id})
+        reset()
+    
+  }
+
+    
+        return (
+            <Form onSubmit={handleSubmit}>
+                <Label>
+                    Name
+                    <Input
+                    type="text"
+                    name="name"
+                    onChange={handleInputChange}
+                    value={name}
+                    pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                    title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                    required
+                />
+                </Label>
+                <Label>
+                    Number
+                <Input
+                    type="tel"
+                    name="number"
+                    onChange={handleInputChange}
+                    value={number}
+                    pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                    title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                    required
+                />
+                </Label>
+                <Button type="submit">Add contact</Button>
+            </Form>
+        )
+    
+}
+
+ContactsForm.propTypes = {
+    onSubmitProp: PropTypes.func.isRequired
+}
+
+export default ContactsForm
+
+/*class ContactsForm extends React.Component {
     state = {
         name: '',
         id: '',
@@ -64,4 +139,4 @@ ContactsForm.propTypes = {
     onSubmitProp: PropTypes.func.isRequired
 }
 
-export default ContactsForm
+export default ContactsForm*/
